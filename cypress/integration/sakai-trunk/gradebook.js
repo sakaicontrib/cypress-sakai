fdf922bcc1ee1b21283b73039fa5b0cbf89f2ab2
Sam Ottenhoff <ottenhoff@longsight.com>
2020-02-06 14:06:50 -0500

    Create a gradebook

---
+++ cypress/integration/sakai-trunk/gradebook.js
@@ -0,0 +1,61 @@
+
+describe('Gradebook', function () {
+    const username = 'instructor1'
+    let siteUrl = ''
+
+    context('Create site and add gradebook', function () {
+
+        before(function () {
+            cy.sakaiLogin(username)
+
+            // Go to user Home and create new course site
+            cy.visit('/portal/site/~' + username)
+            cy.get('a').contains('Worksite Setup').click()
+            cy.get('a').contains('Create New Site').click({ force: true })
+            cy.get('input#course').click()
+            cy.get('select').select('Summer 2018')
+            cy.get('input#submitBuildOwn').click()
+            cy.get('a').contains('select anyway').click()
+            cy.get('input#continueButton').click()
+            cy.get('textarea').last().type('Cypress Testing')
+            cy.get('.act input[name="continue"]').click()
+            cy.get('input#sakai\\.gradebookng').should('be.checked')
+            cy.get('.act input[name="Continue"]').click()
+            cy.get('input#continueButton').click()
+            cy.get('input#addSite').click()
+            cy.get('#flashNotif').contains('has been created')
+            cy.get('#flashNotif a')
+                .should('have.attr', 'href').and('include', '/portal/site/')
+                .then((href) => {
+                    siteUrl = href;
+                    cy.visit(href)
+                })
+            cy.get('.Mrphs-toolsNav__menuitem--link').contains('Gradebook').click()
+        })
+
+
+
+        it('Create categories and items', function() {
+            cy.get('a[title="Settings"]').click({ force: true})
+            cy.get('a').contains('Categories').click()
+            cy.get('input[type="radio"]').last().click()
+            for (let i = 0; i < 4; i++) {
+                cy.get('button').contains('Add a category').click()
+                cy.wait(500)
+                cy.get('#settingsCategories table').find('tr.gb-category-row').should('have.length', (i+2))
+            }
+            cy.get('.gb-category-row input[name$="name"]').first().type('A')
+            cy.get('.gb-category-weight input[name$="weight"]').first().clear().type('10')
+            cy.get('.gb-category-row input[name$="name"]').eq(1).type('B')
+            cy.get('.gb-category-weight input[name$="weight"]').eq(1).clear().type('35')
+            cy.get('.gb-category-row input[name$="name"]').eq(2).type('C')
+            cy.get('.gb-category-weight input[name$="weight"]').eq(2).clear().type('10')
+            cy.get('.gb-category-row input[name$="name"]').eq(3).type('D')
+            cy.get('.gb-category-weight input[name$="weight"]').eq(3).clear().type('15')
+            cy.get('.gb-category-row input[name$="name"]').eq(4).type('E')
+            cy.get('.gb-category-weight input[name$="weight"]').eq(4).clear().type('30')
+            cy.get('.act input[type="button"]').first().click()
+
+        })
+    })
+})
