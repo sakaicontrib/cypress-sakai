deb07979c86d8e580f8c0ea34bf76469adc2f82c
Sam Ottenhoff <ottenhoff@longsight.com>
2020-02-10 20:47:00 -0500

    Gradebook test now working locally after SAK-43202 fix

--- cypress/integration/sakai-trunk/gradebook.js
+++ cypress/integration/sakai-trunk/gradebook.js
@@ -43,15 +43,13 @@ describe('Gradebook', function () {
             cy.get('.Mrphs-toolsNav__menuitem--link').contains('Gradebook').click()
         })
 
-
-
         it('Create categories and items', function() {
             cy.get('a[title="Settings"]').click({ force: true})
             cy.get('a').contains('Categories').click()
             cy.get('input[type="radio"]').last().click()
             for (let i = 0; i < 4; i++) {
-                cy.get('button').contains('Add a category').click()
-                cy.wait(500)
+                cy.get('#gradebookSettings button').contains('Add a category').click()
+                cy.wait(200)
                 cy.get('#settingsCategories table').find('tr.gb-category-row').should('have.length', (i+2))
             }
             cy.get('.gb-category-row input[name$="name"]').first().type('A')
@@ -69,14 +67,15 @@ describe('Gradebook', function () {
             // Now create the gb items
             cy.reload()
             cy.get('input[name="cancel"]').click()
-            for (let i = 0; i < 4; i++) {
-                cy.get('button').contains('Add Gradebook Item').click()
-                cy.get('input[name$="title"]').type(i+1)
-                cy.get('input[name$="points"]').type(100)
-                cy.get('select[name$="category"] > option').eq(i).then(element => cy.get('select').select(element.val()))
-                cy.get('button[name$="submit"]').click()
-            }
 
+            for (let i = 0; i < 5; i++) {
+                cy.get('button').contains('Add Gradebook Item').click()
+                cy.get('.wicket-modal input[name$="title"]').type(1 + i)
+                cy.get('.wicket-modal input[name$="points"]').type(100)
+                cy.get('.wicket-modal select[name$="category"] > option').eq(1 + i).then(element => cy.get('.wicket-modal select[name$="category"]').select(element.val()))
+                cy.get('.wicket-modal button[name$="submit"]').click()
+                cy.wait(200)
+            }
         })
     })
 })
