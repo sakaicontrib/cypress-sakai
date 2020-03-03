9a8000d2833d9cd1c5b3627065bdf0ce7b629ff5
Sam Ottenhoff <ottenhoff@longsight.com>
2020-03-03 17:55:44 -0500

    test deleting a question

--- cypress/integration/sakai-trunk/samigo.js
+++ cypress/integration/sakai-trunk/samigo.js
@@ -10,9 +10,44 @@ describe('Samigo', function () {
 
     context('Create a Quiz', function () {
 
-
-        it('create the site', function () {
+        it('can create a quiz from scratch', function () {
             cy.get('#authorIndexForm a').contains('Add').click()
+            cy.get('#authorIndexForm\\:title').type('Cypress Quiz')
+            cy.get('#authorIndexForm\\:createnew').click()
+
+            // Add a multiple choice question
+            cy.get('#assessmentForm\\:parts\\:0\\:changeQType').select('Multiple Choice')
+            cy.get('#itemForm\\:answerptr').clear().type('99.99')
+            cy.get('#itemForm textarea').first().type('What is chiefly responsible for the increase in the average length of life in the USA during the last fifty years?')
+            cy.get('#itemForm\\:mcchoices textarea').first().type('Compulsory health and physical education courses in public schools.')
+            cy.get('#itemForm\\:mcchoices textarea').eq(1).type('The reduced death rate among infants and young children.')
+            cy.get('#itemForm\\:mcchoices textarea').eq(2).type('The substitution of machines for human labor.')
+            cy.get('#itemForm\\:mcchoices input[type="radio"]').eq(1).click()
+            cy.get('input[type="submit"]').contains('Save').click()
+
+            // Edit the first question
+            cy.get('#assessmentForm\\:parts\\:0\\:parts\\:0\\:answerptr').should('have.value', '99.99')
+            cy.get('#assessmentForm\\:parts\\:0\\:parts\\:0\\:modify').click()
+            cy.get('#itemForm\\:answerptr').clear().type('100.00')
+            cy.get('#itemForm\\:mcchoices textarea').eq(3).type('Safer cars.')
+            cy.get('input[type="submit"]').contains('Save').click()
+
+            // Add a second question
+            cy.get('#assessmentForm\\:parts\\:0\\:changeQType').select('Multiple Choice')
+            cy.get('#itemForm\\:answerptr').clear().type('100.00')
+            cy.get('#itemForm textarea').first().type('What is the main reason so many people moved to California in 1849?')
+            cy.get('#itemForm\\:mcchoices textarea').first().type('California land was fertile, plentiful, and inexpensive.')
+            cy.get('#itemForm\\:mcchoices textarea').eq(1).type('Gold was discovered in central California.')
+            cy.get('#itemForm\\:mcchoices textarea').eq(2).type('The east was preparing for a civil war.')
+            cy.get('#itemForm\\:mcchoices textarea').eq(3).type('They wanted to establish religious settlements.')
+            cy.get('#itemForm\\:mcchoices input[type="radio"]').eq(1).click()
+            cy.get('input[type="submit"]').contains('Save').click()
+            cy.get('#assessmentForm\\:parts').find('.samigo-question-callout').should('have.length', 2)
+
+            // Delete the first question and confirm we only have one question
+            cy.get('#assessmentForm\\:parts\\:0\\:parts\\:0\\:deleteitem').click()
+            cy.get('input[type="submit"]').contains('Remove').click()
+            cy.get('#assessmentForm\\:parts').find('.samigo-question-callout').should('have.length', 1)
         })
     })
 });
